bcb86625d2c61b7e7e8b99f55e9d696e58d4dfa7
mfeyzayildirim@gmail.com <mfeyzayildirim@gmail.com>
2023-11-09 01:19:43 +0300

    end

---
+++ app.js
@@ -0,0 +1,26 @@
+function addTask() {
+  const task = document.getElementById("task");
+  const chooseInput = document.querySelector("#choose");
+  const dateInput = document.querySelector("#date");
+  const taskTitle = task.value.trim(); 
+  const choose = chooseInput.value;
+  const date = dateInput.value;
+
+  if (taskTitle !== "") { 
+    let taskList = document.getElementById("tasklist");
+    let newTask = document.createElement("li");
+    newTask.innerHTML =
+      `${taskTitle} (choose: ${choose}, date: ${date})
+      <button onclick="removeTask(this)">Delete</button>`;
+
+    taskList.appendChild(newTask);
+    task.value = "";
+    chooseInput.value = "easy"; 
+    dateInput.value = "";
+  }
+}
+
+function removeTask(button) {
+  let taskItem = button.parentElement;
+  taskItem.remove(); 
+}
